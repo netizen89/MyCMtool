b6e164531ddaa4a8c89c04df3e0e2b270e65e352
netizen89 <57847458+netizen89@users.noreply.github.com>
2020-02-27 00:40:03 +1100

    Updating/adding logging for the configuring file

--- configure_server.py
+++ configure_server.py
@@ -7,46 +7,80 @@ from sys import argv
 
 import yaml
 import socket
+import logging
 from ssh2.session import Session
 
+#set up log level
+logging.basicConfig(level=logging.INFO)
 
-from pip._vendor.pep517.compat import FileNotFoundError
 
 
-#Config params
-SERVER= []
-USERNAME = ""
-PASSWORD = ""
+# Config params
+SERVER = []
+USERNAME = " "
+PASSWORD = " "
 TASKS = []
 
+def listToString(s):
+    """
+           Function to convert list to string
+           :param : list
+           :return: string
+    """
+    str1 = ""
+    #traverse in list
+    for ele in s:
+        str1 += ele
+        str1.strip('\n')
+
+    return str1
+
+
 def configure_server():
+    """
+        Function to configure servers
+        :param :
+
+    """
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((SERVER[0], 22))
+
+
+    sock.connect((listToString(SERVER[0]), 22))
     # assign to session class
     session = Session()
 
     # set up 3 way handshake
+    logging.info('Setting up 3-way handshake')
     session.handshake(sock)
-    session.userauth_password(USERNAME, PASSWORD)
+    logging.debug('Username is %s', USERNAME)
+    logging.debug('password %s', PASSWORD)
 
+
+    session.userauth_password(USERNAME, PASSWORD)
     channel = session.open_session()
-    channel.execute("uname -a")
+    channel.execute("apt-get update -y; apt-get install apache2 -y; systemctl start apache2.service")
     size, data = channel.read()
-    while (size > 0):
-        print(data.decode())
-        size, data = channel.read()
+    # while (size > 0):
+    #     print(data.decode())
+    #     size, data = channel.read()
 
     channel.close()
     print("Exit status : {0}", format(channel.get_exit_status()))
 
 
+def parse_configuration(f, docs):
+    """
+               Function to parse yaml configuration file
+               :param : file,attributes
 
+    """
 
-def parse_configuration(f, docs) :
+    global USERNAME
+    global PASSWORD
     for doc in docs:
 
         for key, value in doc.items():
-            print(key, "->", value)
+            logging.debug(key, "->", value)
 
             if key == "hosts" and value[0] == "webservers":
                 with open(r'C:\Users\rajpo\PycharmProjects\SlackProj\hosts')as hostfile:
@@ -54,48 +88,45 @@ def parse_configuration(f, docs) :
                     for line in hostfile:
                         if 'webservers' in line:
                             for line in hostfile:
-                                SERVER.append(line)
-                                print("Servers are", SERVER)
+                                SERVER.append(line.strip().split('\t'))
+
+                                logging.debug('Servers are %s', SERVER)
 
             if key == "username":
-                UNAME = value
-                print('Username is ',UNAME[0])
+                USERNAME = listToString(value)
+                logging.debug('Username is %s', USERNAME)
+
             if key == "password":
-                PASSWORD = value
-                print("password %s", PASSWORD)
+                PASSWORD = listToString(value)
+
             if key == "tasks":
                 for line in f:
                     TASKS.append(line)
-                    print(TASKS)
+                    logging.debug('Tasks are %s', TASKS)
 
-        #configure servers
-      #  configure_server()
+        # configure servers
+    configure_server()
 
 
-
-def main(var) :
+def main():
     """
-        main function
+            main function
     """
 
     # access config file
-    print(var)
-    try :
-        with open(r'C:\Users\rajpo\PycharmProjects\SlackProj\config.yml') as f:
-         docs = yaml.load_all(f, Loader=yaml.FullLoader)
-         # function to parse yaml file
-         print(docs)
-         parse_configuration(f, docs)
-    except FileNotFoundError :
-      
+
+    try:
+
+        with open(r'configuration.yml') as f:
+            docs = yaml.load_all(f, Loader=yaml.FullLoader)
+            # function to parse yaml file
+            print(docs)
+            parse_configuration(f, docs)
+    except IOError:
+        logging.error('Cannot find Configuration file -configuration.yml')
+
         exit()
 
 
-
-
-
-
-
-if __name__ == "__main__" :
-    
-    main(argv[0])
+if __name__ == "__main__":
+    main()
